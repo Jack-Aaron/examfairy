4fa44b2c21bd6d1951f1e2fa9a3477783252c36f
Jonathan Alpart <panjialang@gmail.com>
2021-05-16 19:16:20 -0500

    code copied from other branch

--- src/App.js
+++ src/App.js
@@ -7,11 +7,21 @@ import shuffle from './shuffle';
 import './App.css';
 
 const App = () => {
+  // prep local storage
+  const [progressReport, setProgressReport] = useState();
+  const writeLocalStorage = (data) => localStorage.setItem('progress_report', JSON.stringify(data));
   // prepares questions & answers and sets state
   useEffect(() => prepareQuestions(), []);
   const [questionsState, setQuestionsState] = useState();
   const prepareQuestions = () => {
-    let shuffledQuestions = shuffle(QUESTIONS); // input of a question set
+         // set up local storage
+  if (localStorage['progressReport'] === undefined) {
+    setProgressReport(QUESTIONS);
+    localStorage.setItem('progressReport', JSON.stringify(QUESTIONS))
+  } else {
+    setProgressReport(JSON.parse(localStorage['progressReport']));
+  }
+    let shuffledQuestions = shuffle(QUESTIONS); // paramater is input of a question set
     shuffledQuestions = shuffledQuestions.map((question) => {
       shuffle(question.answers); // shuffles answers within the questions
       setQuestionsState(shuffledQuestions)
